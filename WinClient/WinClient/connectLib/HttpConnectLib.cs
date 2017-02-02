59c1c2d3c3d111246b979bead9d957fe7808f42d
kazenetu <rakudafactory@gmail.com>
2017-02-02 20:22:09 +0900

    不要なusingを削除

--- WinClient/WinClient/connectLib/HttpConnectLib.cs
+++ WinClient/WinClient/connectLib/HttpConnectLib.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
-using Newtonsoft.Json;
 
 namespace WinClient.connectLib
 {
